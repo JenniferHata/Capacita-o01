0183394b9b42a137672aeaaf2af85836ca5e9f11
Jennifer Hata <JenniferHata>
2021-09-26 23:09:56 -0300

    Capacitacao

---
+++ gerenciador-de-tarefas/src/app/tarefas/cadastrar/cadastrar-tarefa.component.spec.ts
@@ -0,0 +1,20 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CadastrarTarefaComponent } from './cadastrar-tarefa.component';
+
+describe('CadastrarTarefaComponent', () => {
+  let component: CadastrarTarefaComponent;
+  let fixture: ComponentFixture<CadastrarTarefaComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CadastrarTarefaComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    
+  });
+
+});
---
+++ gerenciador-de-tarefas/src/app/tarefas/editar/editar-tarefa.component.spec.ts
@@ -0,0 +1,20 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditarTarefaComponent } from './editar-tarefa.component';
+
+describe('EditarTarefaComponent', () => {
+  let component: EditarTarefaComponent;
+  let fixture: ComponentFixture<EditarTarefaComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EditarTarefaComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    
+  });
+
+});
